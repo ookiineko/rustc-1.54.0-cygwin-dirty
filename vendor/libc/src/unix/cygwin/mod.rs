2c9fe0b7036452f69848225338519e6f150bcf4b
Ookiineko <chiisaineko@protonmail.com>
2022-10-28 17:37:15 +0800

    sockaddr_in
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- vendor/libc/src/unix/cygwin/mod.rs
+++ vendor/libc/src/unix/cygwin/mod.rs
@@ -225,6 +225,21 @@ s! {
         pub sa_data: [::c_char; 14],
     }
 
+    pub struct sockaddr_in {
+        pub sin_family: sa_family_t,
+        pub sin_port: ::in_port_t,
+        pub sin_addr: ::in_addr,
+        pub sin_zero: [u8; 8],
+    }
+
+    pub struct sockaddr_in6 {
+        pub sin6_family: sa_family_t,
+        pub sin6_port: ::in_port_t,
+        pub sin6_flowinfo: u32,
+        pub sin6_addr: ::in6_addr,
+        pub sin6_scope_id: u32,
+    }
+
     pub struct msghdr {
         pub msg_name: *mut ::c_void,
         pub msg_namelen: ::socklen_t,
