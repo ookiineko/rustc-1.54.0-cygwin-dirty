dfe9608816e49a9d190610c85dfa07d14b2cf490
Ookiineko <chiisaineko@protonmail.com>
2022-10-28 17:44:55 +0800

    tm
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- vendor/libc/src/unix/cygwin/mod.rs
+++ vendor/libc/src/unix/cygwin/mod.rs
@@ -264,6 +264,20 @@ s! {
         pub ai_next: *mut addrinfo,
     }
 
+    pub struct tm {
+        pub tm_sec: ::c_int,
+        pub tm_min: ::c_int,
+        pub tm_hour: ::c_int,
+        pub tm_mday: ::c_int,
+        pub tm_mon: ::c_int,
+        pub tm_year: ::c_int,
+        pub tm_wday: ::c_int,
+        pub tm_yday: ::c_int,
+        pub tm_isdst: ::c_int,
+        pub tm_gmtoff: ::c_long,
+        pub tm_zone: *const ::c_char,
+    }
+
     pub struct msghdr {
         pub msg_name: *mut ::c_void,
         pub msg_namelen: ::socklen_t,
