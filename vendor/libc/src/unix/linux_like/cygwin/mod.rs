94b5d354e73c8b2a08ab6a85185970a14c21de08
Ookiineko <chiisaineko@protonmail.com>
2022-10-28 15:31:42 +0800

    pthread_rwlockattr_t
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- vendor/libc/src/unix/linux_like/cygwin/mod.rs
+++ vendor/libc/src/unix/linux_like/cygwin/mod.rs
@@ -111,6 +111,10 @@ s! {
     pub struct pthread_mutexattr_t {
         __dummy: ::c_char,
     }
+
+    pub struct pthread_rwlockattr_t {
+        __dummy: ::c_char,
+    }
 }
 
 pub type statfs64 = statfs;
