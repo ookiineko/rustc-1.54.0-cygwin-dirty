3ed17507cd6834fa81e722d83e557894b09f6f9c
Ookiineko <chiisaineko@protonmail.com>
2022-10-29 20:33:28 +0800

    ??
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- library/std/src/os/unix/net/ancillary.rs
+++ library/std/src/os/unix/net/ancillary.rs
@@ -424,13 +424,14 @@ impl<'a> Iterator for Messages<'a> {
             let mut msg: libc::msghdr = zeroed();
             msg.msg_control = self.buffer.as_ptr() as *mut _;
             cfg_if::cfg_if! {
-                if #[cfg(any(target_os = "android", all(target_os = "linux", target_env = "gnu"), target_os = "cygwin"))] {
+                if #[cfg(any(target_os = "android", all(target_os = "linux", target_env = "gnu")))] {
                     msg.msg_controllen = self.buffer.len() as libc::size_t;
                 } else if #[cfg(any(
                               target_os = "dragonfly",
                               target_os = "emscripten",
                               target_os = "freebsd",
                               all(target_os = "linux", target_env = "musl",),
+                              target_os = "cygwin",
                               target_os = "macos",
                               target_os = "netbsd",
                               target_os = "openbsd",
