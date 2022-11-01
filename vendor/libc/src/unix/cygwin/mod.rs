509d9979810928983a89420d44e4feee59d7d8e4
Ookiineko <chiisaineko@protonmail.com>
2022-11-01 17:21:49 +0800

    oops
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- vendor/libc/src/unix/cygwin/mod.rs
+++ vendor/libc/src/unix/cygwin/mod.rs
@@ -1899,7 +1899,7 @@ extern "C" {
         fd: ::c_int,
         times: [*const ::timespec; 2]
     ) -> ::c_int;
-    pub fn futimens(fd: ::c_int, times: *const ::timespec) -> ::c_int;
+    pub fn futimens(fd: ::c_int, times: [*const ::timespec; 2]) -> ::c_int;
 
     /* stdlib.h */
 
